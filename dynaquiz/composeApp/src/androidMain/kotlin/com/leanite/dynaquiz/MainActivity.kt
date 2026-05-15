42675c0b631129ac0b7edb15cb478331a88b150e
leanite <cleite.leandro@gmail.com>
2026-05-15 12:20:37 -0300

    feat: change status bar color to night mode

--- dynaquiz/composeApp/src/androidMain/kotlin/com/leanite/dynaquiz/MainActivity.kt
+++ dynaquiz/composeApp/src/androidMain/kotlin/com/leanite/dynaquiz/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.leanite.dynaquiz
 
+import android.app.UiModeManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
@@ -12,7 +14,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(UiModeManager.MODE_NIGHT_YES),
+            navigationBarStyle = SystemBarStyle.dark(UiModeManager.MODE_NIGHT_YES),
+        )
         setContent { App() }
     }
 }
